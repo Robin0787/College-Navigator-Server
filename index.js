b8f9ed0ae01efed99daa786803750f49dbb04369
Robin <mohammadrobin636@gmail.com>
2023-07-22 18:32:19 +0600

    connected to mongoDB and created colleges api

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3010;
-
+require('dotenv').config();
 
 app.use(express.json());
 app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.egzybej.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const collegeCollection = client.db('College-Navigator').collection('Colleges');
+
+    // GET ------ GET ------ GET -------- GET
+    app.get('/colleges', async(req, res) => {
+        const colleges = await collegeCollection.find().toArray();
+        res.send(colleges);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 
